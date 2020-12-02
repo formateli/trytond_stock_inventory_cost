b7bfd18bf7fe2dfbe515a5e9f0da1aced6ac0024
formateli <formateli@gmail.com>
2020-12-01 20:09:14 -0500

    Line list simple

--- inventory.py
+++ inventory.py
@@ -1,13 +1,10 @@
 # This file is part of trytond-stock_inventory_cost module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
 from trytond.pool import PoolMeta
 from trytond.model import fields
 from decimal import Decimal
 from trytond.modules.product import price_digits
-
-__all__ = ['Inventory', 'InventoryLine']
 
 
 class Inventory(metaclass=PoolMeta):
@@ -15,18 +12,26 @@ class Inventory(metaclass=PoolMeta):
 
     total_cost = fields.Function(
         fields.Numeric('Total Cost', digits=price_digits),
-            'get_total_cost')
+            'on_change_with_total_cost')
 
     @fields.depends('lines')
-    def on_change_lines(self):
-        self.total_cost = self.get_total_cost()
-
-    def get_total_cost(self, name=None):
+    def on_change_with_total_cost(self, name=None):
         result = Decimal('0.0')
         for line in self.lines:
             if line.total_cost:
                 result += line.total_cost
         return result
+
+#    @fields.depends('lines')
+#    def on_change_lines(self):
+#        self.total_cost = self.get_total_cost()
+
+#    def get_total_cost(self, name=None):
+#        result = Decimal('0.0')
+#        for line in self.lines:
+#            if line.total_cost:
+#                result += line.total_cost
+#        return result
 
     @classmethod
     def complete_lines(cls, inventories, fill=True):
@@ -50,15 +55,24 @@ class InventoryLine(metaclass=PoolMeta):
             digits=price_digits)
     total_cost = fields.Function(
         fields.Numeric('Total Cost', digits=price_digits),
-            'get_total_cost')
+            'on_change_with_total_cost')
 
+    @fields.depends('cost_price')
+    def on_change_with_total_cost(self, name=None):
+        if self.cost_price is None or \
+                self.diff_quantity is None:
+            return
+        exp = Decimal(str(10.0 ** -self.unit_digits))
+        result = Decimal(self.diff_quantity) * self.cost_price
+        return result.quantize(exp)
+
+    @fields.depends('product')
     def on_change_product(self):
         super(InventoryLine, self).on_change_product()
         self.set_cost_price()
 
     @fields.depends('product')
     def on_change_quantity(self):
-        super(InventoryLine, self).on_change_quantity()
         self.set_cost_price()
 
     def set_cost_price(self):
@@ -66,12 +80,12 @@ class InventoryLine(metaclass=PoolMeta):
         self.total_cost = None
         if self.product:
             self.cost_price = self.product.template.cost_price
-            self.total_cost = self.get_total_cost()
+            #self.total_cost = self.get_total_cost()
 
-    def get_total_cost(self, name=None):
-        if self.cost_price is None or \
-                self.diff_quantity is None:
-            return
-        exp = Decimal(str(10.0 ** -self.unit_digits))
-        result = Decimal(self.diff_quantity) * self.cost_price
-        return result.quantize(exp)
+#    def get_total_cost(self, name=None):
+#        if self.cost_price is None or \
+#                self.diff_quantity is None:
+#            return
+#        exp = Decimal(str(10.0 ** -self.unit_digits))
+#        result = Decimal(self.diff_quantity) * self.cost_price
+#        return result.quantize(exp)
